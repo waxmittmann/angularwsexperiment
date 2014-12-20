1599ab1a5ae68a34446d4168871757a5d9bd524b
Maximilian Wittmann <damxam@gmail.com>
2014-12-20 14:52:17 +1100

    Random strategy working pretty OK. Move on to question strategy.

--- public/resources/js/ImageRotatorController.js
+++ public/resources/js/ImageRotatorController.js
@@ -9,22 +9,18 @@
       var registeredDirectiveRotation = {};
       $scope.state = "initial";
 
-      // console.log("Here state is " + $scope.attrs.state);
-      // $scope.$watch($scope.state, function (newState) {
+      var getRandomImage = function() {
+        return $scope.images[Math.floor(Math.random() * $scope.images.length)];
+      }
 
-      // $scope.$watch('state', function (newState) {
-      //   console.log("Got state update, it is ", newState, " and ", $scope.state);
-      //   $scope.state = newState;
-      // });
+      var imageSetup = function() {
+          for(var i = 0; i < registeredDirectives.length; i++) {
+            registeredDirectives[i].setup(getRandomImage(), getRandomImage());
+          }
+      };
 
       var imageSwitcher = function() {
-        var randomSwitcher = (function() {
-          // function getRandomDirective() {
-          //   var at = Math.floor(Math.random() * registeredDirectives.length);
-          //   console.log("Got " + at + " with length of " + registeredDirectives.length);
-          //   return registeredDirectives[at];
-          // }
-
+        var randomSwitcher = function(activeState) {
           function getImageOrder() {
             console.log("Getting image order");
             var directivesLeft = registeredDirectives.slice(0);
@@ -32,7 +28,6 @@
             while(directivesLeft.length > 0) {
               var at = Math.floor(Math.random() * directivesLeft.length);
               imageOrder.push(directivesLeft[at]);
-              // directivesLeft.remove(at);
               directivesLeft.splice(at, 1);
             }
             console.log("Image order that will be returned is ", imageOrder);
@@ -41,31 +36,20 @@
 
           function executeImageSwitches(imageOrder) {
             console.log("Executing image switches with order: ", imageOrder, ". Total directives: ", registeredDirectives.length);
-            // for(var i = 0; i < imageOrder.length; i++) {
-            //   var image = $scope.images[Math.floor(Math.random() * $scope.images.length)];
-            //   imageOrder[i].changeImage(image);
-            // }
-            if(imageOrder.length == 0) {
+            if(imageOrder.length == 0 || $scope.state != activeState) {
               console.log("All done with switches");
               $timeout(changeImage, 500);
             } else {
-              console.log("Switching1");
+              console.log("Switching");
               var image = $scope.images[Math.floor(Math.random() * $scope.images.length)];
-              console.log("Switching2");
-              var at = Math.floor(Math.random() * imageOrder.length);
-              console.log("Switching3");
-              imageOrder[at].changeImage(image);
-              console.log("Switching4");
-              // imageOrder.remove(i);
-              imageOrder.splice(at, 1);
-              console.log("Switching5");
+              imageOrder[0].changeImage(image);
+              imageOrder.splice(0, 1);
               $timeout(
                 function() {
                   executeImageSwitches(imageOrder);
                 },
                 500);
             }
-            // getRandomDirective().changeImage(image);
           }
 
           return {
@@ -74,24 +58,32 @@
               executeImageSwitches(imageOrder);
             }
           };
-        });
+        };
 
-        var questionSwitcher = (function() {
-          $timeout(changeImage, 100);
-        })();
+        var questionSwitcher = function() {
+          function executeImageSwitches() {
+            $timeout(executeImageSwitches, 100);
+          }
 
-        var answerSwitcher = (function() {
+          return {
+            'switch': function() {
+              executeImageSwitches();
+            }
+          }
+        };
+
+        var answerSwitcher = function() {
           $timeout(changeImage, 100);
-        }());
+        };
 
         var changeImage = function() {
             console.log("ChangeImage");
             if($scope.state == 'initial') {
-              randomSwitcher().switch();
+              randomSwitcher('initial').switch();
             } else if($scope.state == 'question') {
-              questionSwitcher.switch();
+              questionSwitcher('question').switch();
             } else if($scope.state == 'answer') {
-              answerSwitcher.switch();
+              answerSwitcher('answer').switch();
             } else {
               $timeout(changeImage, 2000);
               throw "Bad state " + $scope.state;
@@ -99,42 +91,6 @@
         };
         changeImage();
       };
-
-      /*
-      var randomImageChanger = function(directiveToUpdate, $scope) {
-        var newImageA = getNewImage($scope.images, directiveToUpdate.imageDataA, directiveToUpdate.imageDataB);
-        var newImageB = getNewImage($scope.images, directiveToUpdate.imageDataA, directiveToUpdate.imageDataB);
-        directiveToUpdate.setup(newImageA, newImageB);
-
-        function getNewImage(images, curFrontImage, curBackImage) {
-          var newImage = images[Math.floor(Math.random() * images.length)];
-          while(newImage == curFrontImage || newImage == curBackImage) {
-            newImage = images[Math.floor(Math.random() * images.length)];
-          }
-          return newImage;
-        }
-
-        function getTimeToNext() {
-          return Math.floor(2000 + Math.random() * 2000);
-        }
-
-        var changeImages = function() {
-          if(debug)
-            console.log("ChangeImages called");
-          try {
-            if($scope.images !== "undefined") {
-              if(debug)
-                console.log("Pre-change");
-              var newImage = getNewImage($scope.images, directiveToUpdate.imageDataA, directiveToUpdate.imageDataB);
-              directiveToUpdate.changeImage(newImage);
-              $timeout(changeImages, getTimeToNext());
-            }
-          } catch (err) {
-            console.log(err);
-          }
-        };
-        changeImages();
-      };*/
 
       //Methods
       function createApi() {
@@ -144,9 +100,6 @@
             if(debug)
               console.log("Registered " + directive);
             registeredDirectives.push(directive);
-            // if($scope.images) {
-            //   randomImageChanger(directive, $scope);
-            // }
             return idAt++;
           }
         };
@@ -156,9 +109,7 @@
         $http.get('/images')
           .success(function(data, status, headers, config) {
               $scope.images = data;
-              // for(var i = 0; i < registeredDirectives.length; i++) {
-              //   imageChanger(registeredDirectives[i], $scope);
-              // }
+              imageSetup();
               imageSwitcher();
               if(debug)
                 console.log("Had success with " + data);
