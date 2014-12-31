1dfa20e1f9a8f29c81b5efa41c16736de3d61c7e
Maximilian Wittmann <damxam@gmail.com>
2014-12-31 11:56:03 +1100

    Slightly speed up anim

--- public/resources/js/ImageRotatorController.js
+++ public/resources/js/ImageRotatorController.js
@@ -251,7 +251,7 @@
                 function() {
                   executeImageSwitches(imageOrder);
                 },
-                150);
+                120);
               }
             }
 
